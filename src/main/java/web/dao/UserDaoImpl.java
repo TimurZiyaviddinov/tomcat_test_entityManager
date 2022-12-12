10a578ce4a220f9531d627e45cda5ad12426a19a
TimurZiyaviddinov <timka_ziya@mail.ru>
2022-12-12 11:38:08 +0300

    HiberConfig

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -5,38 +5,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import web.model.User;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
 public class UserDaoImpl implements UserDao {
-    @Autowired
-    private SessionFactory sessionFactory;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Override
     public void addUser(User user) {
-        sessionFactory.getCurrentSession().persist(user);
+        entityManager.persist(user);
+        //sessionFactory.getCurrentSession().persist(user);
     }
 
     @Override
     public User getUserById(int id) {
-        return sessionFactory.getCurrentSession().get(User.class, id);
+        return entityManager.find(User.class, id);
+//        return sessionFactory.getCurrentSession().get(User.class, id);
     }
 
     @Override
     public void updateUser(User user) {
-        sessionFactory.getCurrentSession().update(user);
+        entityManager.merge(user);
+//        sessionFactory.getCurrentSession().update(user);
     }
 
     @Override
     public void deleteUser(User user) {
-        sessionFactory.getCurrentSession().remove(user);
+        entityManager.remove(entityManager.find(User.class, user.getId()));
+//        sessionFactory.getCurrentSession().remove(user);
     }
 
     @Override
-    @SuppressWarnings("unchecked")
+//    @SuppressWarnings("unchecked")
     public List<User> listUsers() {
-        TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+        TypedQuery<User> query = entityManager.createQuery("select a from User a", User.class);
         return query.getResultList();
+//        TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+//        return query.getResultList();
     }
 }
